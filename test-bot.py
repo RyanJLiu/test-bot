0dafd6574fa3891174d375377dff014132ec5c20
Ryan <juihe.liu@gmail.com>
2021-12-29 11:52:05 +0800

    retesting

--- test-bot.py
+++ test-bot.py
@@ -1,7 +1,5 @@
 from flask import Flask, request, abort
 
-import re
-
 from linebot import (
     LineBotApi, WebhookHandler
 )
@@ -11,7 +9,6 @@ from linebot.exceptions import (
 from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
 )
-from linebot.models import *
 
 app = Flask(__name__)
 
@@ -40,15 +37,7 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    msg = TextSendMessage(text=event.massage.text) # 使用者輸入的內容
-    #profile = line_bot_api.get_profile(event.source.user_id)
-    #user_name = profile.display_name #使用者名稱
-    #uid = profile.user_id # 發訊者ID
-    #if re.match("test", msg):
-    #    line_bot_api.push_message(uid, TextSendMessage("test success"))
-    #elif re.match("測試", msg):
-    #    line_bot_api.push_message(uid, TextSendMessage("測試成功"))
-    #else:
+    msg = TextSendMessage(text=event.massage.text)
     line_bot_api.reply_message(event.reply_token, msg)
 
 import os
