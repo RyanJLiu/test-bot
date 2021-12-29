a0692fdc194a558e43aa3e41f146fa97847cd28a
Ryan <juihe.liu@gmail.com>
2021-12-29 11:57:49 +0800

    retest

--- test-bot.py
+++ test-bot.py
@@ -37,7 +37,7 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    msg = TextSendMessage(text=event.massage.text)
+    msg = TextSendMessage(text=event.message.text)
     line_bot_api.reply_message(event.reply_token, msg)
 
 import os
