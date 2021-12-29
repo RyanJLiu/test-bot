354783320e7beecc8c4988823b539bb0d275fdf0
Ryan <juihe.liu@gmail.com>
2021-12-29 11:39:45 +0800

    revise testing content

--- test-bot.py
+++ test-bot.py
@@ -44,9 +44,9 @@ def handle_message(event):
     profile = line_bot_api.get_profile(event.source.user_id)
     user_name = profile.display_name #使用者名稱
     uid = profile.user_id # 發訊者ID
-    if (msg=="test"):
+    if re.match("test", msg):
         line_bot_api.push_message(uid, TextSendMessage("test success"))
-    elif (msg=="測試"):
+    elif re.match("測試", msg):
         line_bot_api.push_message(uid, TextSendMessage("測試成功"))
     else:
         line_bot_api.reply_message(event.reply_token, msg)
