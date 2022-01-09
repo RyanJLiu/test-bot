b02ceb3363535ec1c92b9fed6b4d2325abc23137
Ryan <juihe.liu@gmail.com>
2022-01-10 02:58:01 +0800

    retry event scheduling

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
 web: python test-bot.py
-clock: python clock.py
---
+++ scheduling.py
@@ -0,0 +1,13 @@
+from linebot import LineBotApi
+from linebot.models import TextSendMessage, ImageSendMessage
+from linebot.exceptions import LineBotApiError
+
+CHANNEL_ACCESS_TOKEN = "UYBnsEo8qLdA4aQAdbX/NUxy9L86NT5epTsoy8d3h2xRItfJyecH49jRkUP26eHbHqN6FCUyboStjuk7TMg/a1u+c+z9mS/CvKJ2/g3my9olG4CQy2yLfHO3ceORUy5CxufKXbUl1xyG7nGHSsM7IwdB04t89/1O/w1cDnyilFU="
+to = "Ud0b3296f8e4a70520b4ed2f2d1b3bdd8"
+
+line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
+
+try:
+    line_bot_api.push_message(to, TextSendMessage(text='Time to sleep.'))
+except LineBotApiError as e:
+    raise e
