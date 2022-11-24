8cf19ec49d897781e43aa7b0749de0202911c03e
Ryan <juihe.liu@gmail.com>
2022-11-24 21:30:08 +0800

    self awakening test

--- clock.py
+++ clock.py
@@ -3,6 +3,11 @@ import scheduling
 
 sched = BlockingScheduler()
 
+@sched.scheduled_job('interval', minutes=30)
+def scheduled_job():
+    url = "https://test-bot-first.herokuapp.com/"
+    conn = urllib.request.urlopen(url)
+
 @sched.scheduled_job('interval', minutes=5)
 def scheduled_job():
     scheduling.med_mind()
